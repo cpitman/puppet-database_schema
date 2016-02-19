6ab6c14650e1d2e62d2a601e3809a7842d416ece
Alexander Fisher <alex@linfratech.co.uk>
2016-02-19 17:32:35 +0000

    Add 'path' fact to fix spec tests
    camptocamp/archive fails if this fact doesn't exist

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
+RSpec.configure do |c|
+  c.default_facts = {
+    :path => '/sbin:/bin:/usr/sbin:/usr/bin:/root/bin'
+  }
+end
+
 shared_examples :compile, :compile => true do
   it { should compile.with_all_deps }
 end
